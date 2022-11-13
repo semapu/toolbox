a48e1acac7edba5f36e5562e00ede1c936f03644
semapu <sergi.mas.pujol@upc.edu>
2022-11-13 15:33:58 +0100

    feat: check input directory

---
+++ tests/notebooks_test.py
@@ -0,0 +1,21 @@
+"""
+Author: Sergi Mas-Pujol
+Last update: 13/11/2022
+
+Test related to functionalities for the Notebooks
+"""
+
+import unittest
+import sys
+sys.path.append('.')
+sys.path.append('..')
+from notebooks.clean_notebooks import clean_notebooks
+
+
+class CleanNotebookTest(unittest.TestCase):
+    def test_clean_notebook_invalid_directory(self):
+        self.assertRaises(ValueError, clean_notebooks, "hola")
+
+
+if __name__ == '__main__':
+    unittest.main()
