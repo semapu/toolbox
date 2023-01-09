c6549c14f52d638400ba42bd630aad4d4b20be88
semapu <sergi.mas.pujol@upc.edu>
2023-01-09 18:37:12 +0100

    feat: add stamp

--- firefox_extensions/borderify/borderify.js
+++ firefox_extensions/borderify/borderify.js
@@ -1,1 +1,5 @@
 document.body.style.border = "5px solid red";
+const collection = document.getElementsByClassName("c-mr106-title");
+console.log(collection[1].textContent)
+document.getElementsByClassName("c-mr106-title")[1].textContent = "SERGI HAS BEEN HERE!!!"
+//document.getElementById("c-mr106-title").className = "SERGI HAS BEEN HERE!!!";
